9c028d06b77f9c25dfab8a6b5d6f68fb7c77f2a0
Aditya <4adi26@gmail.com>
2024-05-14 18:26:58 +0530

    setup multer and cloudinary for file upload

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { app } from "./app.js"
 const PORT = process.env.PORT || 8000;
 
 connectDB()
-.then(() => applisten(PORT, () => {
+.then(() => app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`)
 }))
 .catch((err) => {
